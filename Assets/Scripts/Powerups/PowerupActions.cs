eba6808df7c886beca226ad902cb5267fe2d0585
junekaa06 <junekaa06@gmail.com>
2021-10-17 12:26:12 -0700

    Powerups added to Main Backyard

--- Assets/Scripts/Powerups/PowerupActions.cs
+++ Assets/Scripts/Powerups/PowerupActions.cs
@@ -6,14 +6,30 @@ public class PowerupActions : MonoBehaviour
 {
     [SerializeField]
     private PlayerController playerController;
+    [SerializeField]
+    private GameObject playerObject;
 
     public void HighSpeedStartAction()
     {
-        playerController.playerSpeed *= 2.0f;
+        playerController.playerSpeed = Mathf.Clamp((playerController.playerSpeed *= 2.0f), 10.0f, 20.0f);
+        playerObject.GetComponent<Renderer>().material.color = new Color32(250, 0, 222, 255);
     }
 
     public void HighSpeedEndAction()
     {
-        playerController.playerSpeed /= 2.0f;
+        playerController.playerSpeed = Mathf.Clamp((playerController.playerSpeed /= 2.0f), 10.0f, 20.0f);
+        playerObject.GetComponent<Renderer>().material.color = new Color32(52, 217, 59, 255);
+    }
+
+    public void StrongAttackStartAction()
+    {
+        playerController.attackPower = Mathf.Clamp((playerController.attackPower*= 2), 1, 2);
+        playerObject.GetComponent<Renderer>().material.color = new Color32(0, 46, 255, 255);
+    }
+
+    public void StrongAttackEndAction()
+    {
+        playerController.attackPower = Mathf.Clamp((playerController.attackPower/= 2), 1, 2);
+        playerObject.GetComponent<Renderer>().material.color = new Color32(52, 217, 59, 255);
     }
 }
--- Assets/Scripts/Powerups/PowerupController.cs
+++ Assets/Scripts/Powerups/PowerupController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PowerupController : MonoBehaviour
 {
-
     public GameObject powerupPrefab;
 
     public List<Powerup> powerups;
