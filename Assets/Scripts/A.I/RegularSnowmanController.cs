42dcee22b518cf75f93b4864e403ba8a8746f7ab
johnlim612 <johnlim0612@gmail.com>
2021-11-29 00:18:45 -0800

    reverting faulty push

--- Assets/Scripts/A.I/RegularSnowmanController.cs
+++ Assets/Scripts/A.I/RegularSnowmanController.cs
@@ -5,63 +5,68 @@ using UnityEngine;
 public class RegularSnowmanController : SnowmanController
 {
     [Header("Attributes")]
+    private float atkCoolDown = 0f;
     public float atkRate = 0.7f;
-    public float atkPower = 100f;
+    public float atkPower = 5f;
     public float knockbackDuration = 0.3f;
 
     [Header("Effects")]
     public GameObject snowParticle;
+    //private Collider currentCollision;
 
+    /// <summary>
+    /// NOTE TO SELF, TRIGGER KNOCK BACK INSIDE ATTACK FUNCTION ONLY WHEN ONTRIGGER HAS HIT PLAYER. 
+    /// CREATE BOOLEAN VALUE TO CHECK THIS IN ATTACK (ISKNOCKED or smthing) THEN TRIGGER KNOCK BACK METHOD INSIDE ATTACK
+    /// AND MAKE SURE ITS SIMULTANEIOUS WITH HEAT LOSS
+    /// </summary>
+    /// <param name="target"></param>
     public override void Attack(Transform target) {
-        GameObject player = target.gameObject;
-        Warmth warmth = player.GetComponent<Warmth>();
-
-        if (isAttacking && !warmth.isDowned && atkCoolDown <= 0f) {
+        if (atkCoolDown <= 0f)
+        {
+            /////////// added and modified code
+            //Player takes damage
             animator.SetTrigger("Attack");
-            knockBack(target, atkPower, knockbackDuration);
 
-            //Do damage if player is not invulnerable
-            if (!player.GetComponent<Warmth>().invulnerable) warmth.removeWarmth(attackDamage, true);
-                
+            target.gameObject.GetComponent<Warmth>().removeWarmth(attackDamage, true);
+            Debug.Log("warmth= " + target.GetComponent<Warmth>().warmth);
+            //////////
+
+            //calculate direction
+            Vector3 dir = target.transform.position - transform.position;
+            dir = -dir;
+
+            //Attacking Player <ANIMATION NEEDED>
+            knockBack(target, dir, atkPower, knockbackDuration);
+
             //reset cooldown
-            atkCoolDown = 1f / atkRate; 
+            atkCoolDown = 1f / atkRate;
         }
+        atkCoolDown -= Time.deltaTime;
     }
 
-    private void knockBack(Transform target, float power, float overTime) {
-        //Attack Particles
-        Vector3 effectLocation = new Vector3(transform.position.x, 0, transform.position.z);
+    /*
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Activated");
+        if (isAttacking && other.gameObject.tag == "Player") {
+            //Player takes damage
+            currentCollision = other;
+            currentCollision.GetComponent<Warmth>().removeWarmth(attackDamage, true);
+            Debug.Log("warmth= " + other.GetComponent<Warmth>().warmth);
+
+            GameObject target = other.gameObject;
+            Attack(target.transform);
+        }
+    }*/
+
+    private void knockBack(Transform target, Vector3 direction, float length, float overTime) {
+        direction = direction.normalized;
+        Vector3 effectLocation = new Vector3(transform.position.x, 0, transform.position.z); 
         GameObject effectIns = (GameObject)Instantiate(snowParticle, effectLocation, transform.rotation);
         Destroy(effectIns, 2f);
-
-        //start of knockback
-        StartCoroutine(knockBackCoroutine(target, power, overTime));
+        StartCoroutine(knockBackCoroutine(target, direction, length, overTime));
     }
 
-    IEnumerator knockBackCoroutine(Transform target, float power, float overTime) {
-        float timeleft = overTime;
-        target.gameObject.GetComponent<CharacterController>().enabled = false;
-
-        //Knockback Animation
-        while (timeleft > 0) {
-            Vector3 moveDirection = transform.position - target.position;
-            target.gameObject.GetComponent<Rigidbody>().AddForce(moveDirection.normalized * -power); 
-            timeleft -= Time.deltaTime;
-            yield return null;
-        }
-        target.gameObject.GetComponent<CharacterController>().enabled = true;
-    }
-
-
-
-
-
-
-
-
-
-/*    OLD TRANSLATE VERSION
- *    IEnumerator knockBackCoroutine(Transform target, Vector3 direction, float length, float overTime) {
+    IEnumerator knockBackCoroutine(Transform target, Vector3 direction, float length, float overTime) {
         float timeleft = overTime;
         while (timeleft > 0) {
             if (timeleft > Time.deltaTime)
@@ -71,6 +76,5 @@ public class RegularSnowmanController : SnowmanController
             timeleft -= Time.deltaTime;
             yield return null;
         }
-        target.gameObject.GetComponent<SplitScreenPlayerController>().isKnocked = false;
-    }*/
+    }
 }
